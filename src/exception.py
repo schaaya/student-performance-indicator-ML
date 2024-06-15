1ac4c74bb0de02eb73c73b9dd5e8f6b05bb45699
schaaya <schaaya@umich.edu>
2024-06-14 22:28:31 -0400

    Create Data-Ingestion Component

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
 import sys
-from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -19,12 +18,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
-
-if __name__ == "__main__":
-
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)
